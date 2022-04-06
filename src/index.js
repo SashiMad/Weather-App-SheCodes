4ebe19455679783e6be5c311911f1ee76f89246d
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-04-06 16:12:22 +0200

    Show current weather description, humidity and wind

--- src/index.js
+++ src/index.js
@@ -25,6 +25,12 @@ function showCityTemp(response) {
   document.querySelector("#currentTemp").innerHTML = Math.round(
     response.data.main.temp
   );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
 }
 let searchedCity = document.querySelector("#search-city");
 searchedCity.addEventListener("submit", search);
